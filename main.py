cac00c56644f651a257d7309f9ff8a6ec4774d49
Timofey Tipishev <tipishev@yandex-team.ru>
2015-05-23 18:36:19 +0500

    add functions to read file and md5 it

--- main.py
+++ main.py
@@ -6,40 +6,53 @@ from __future__ import print_function, unicode_literals, absolute_import
 __author__ = 'tipishev'
 
 from os import walk
+# from md5 import digest as md5sum
+import md5
+from string import punctuation
+from re import sub
 
+# filesystem stuff
 LOGFILE = "main.log"
+DOCS_DIRECTORY = "../docs/"
+
+# algorithm stuff
+NUMBER_OF_DIMENSIONS = 64
+NUMBER_OF_VECTORS = 5
 
 import logging
 logging.basicConfig(filename=LOGFILE, level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
-def count_words(source_directory, output_file):
-    dictionary = dict()
-    for (dirpath, dirnames, filenames) in walk(source_directory):
-        total_length = len(filenames)
-        for count, filename in enumerate(filenames):
-            fullpath = dirpath+filename
-            with open(fullpath, 'r') as f:
-                for word in f.read().split():
-                    if word not in dictionary:
-                        dictionary[word] = 1
-                    else:
-                        dictionary[word] += 1
-                print("Writing {} of {}".format(count, total_length))
+def md5sum(string):
+    return md5.new(string).hexdigest()
 
-    with codecs.open(output_file, 'w', 'utf8') as g:
-        for (key, value) in dictionary.iteritems():
-            # g.write('35')
-            g.write("{} {}\n".format(key.decode('utf8'), value))
+def distance(first, second):
+    assert len(first)==len(second)
+    return sum([a!=b for (a, b) in zip(first, second)])
 
-    # with codecs.open(target_directory+filename, 'w', 'utf8') as g:
-    #     g.write(text)
+def to_words(raw_text):
+    return sub('[{}]'.format(punctuation), ' ', raw_text.lower()).split()
+
+def to_text(filename):
+    with open(filename, 'r') as f:
+        return ' '.join(line for line in f)
+
+def to_binary(hexstring):
+    return bin(int(hexstring, 16))[2:]
+
+def to_hash_sum_vector(filename, length):
+    ''' create a sum vector depending on each word's md5 sum '''
+    result = [0 for _ in range(length)]
+    for word in to_words((to_text(filename))):
+        control_vector = to_binary(md5sum(word))[0:length]
+        for position, char in enumerate(control_vector):
+            result[position] += 1 if char == '1' else -1
+    return result
+
 
 def main():
-    TARGET_DIRECTORY = "../docs/"
-
-    count_words(TARGET_DIRECTORY, 'count')
+    print(to_hash_sum_vector('34567', NUMBER_OF_VECTORS))
 
 if __name__ == '__main__':
     main()
