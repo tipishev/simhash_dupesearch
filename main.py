b737e18cb670938f19e8d0eca1ac28e7bcf631b8
Timofey Tipishev <tipishev@yandex-team.ru>
2015-05-23 19:21:27 +0500

    fix hex to binary conversion to keep leading zeros

--- main.py
+++ main.py
@@ -17,15 +17,14 @@ DOCS_DIRECTORY = "../docs/"
 
 # algorithm stuff
 NUMBER_OF_DIMENSIONS = 64
-NUMBER_OF_VECTORS = 5
+NUMBER_OF_VECTORS = 8
 
 import logging
 logging.basicConfig(filename=LOGFILE, level=logging.INFO)
 log = logging.getLogger(__name__)
 
-
 def md5sum(string):
-    return md5.new(string).hexdigest()
+    return md5.new(string.encode('utf8')).hexdigest()
 
 def distance(first, second):
     assert len(first)==len(second)
@@ -36,23 +35,30 @@ def to_words(raw_text):
 
 def to_text(filename):
     with open(filename, 'r') as f:
-        return ' '.join(line for line in f)
+        return ' '.join(line.decode('utf8') for line in f)
 
-def to_binary(hexstring):
-    return bin(int(hexstring, 16))[2:]
+def to_binary(hexstring, length_in_bits=128):
+    return bin(int(hexstring, 16))[2:].zfill(length_in_bits)
 
-def to_hash_sum_vector(filename, length):
+def file_to_words(filename):
+    return to_words((to_text(filename)))
+
+def to_hash_vector(words, length):
     ''' create a sum vector depending on each word's md5 sum '''
     result = [0 for _ in range(length)]
-    for word in to_words((to_text(filename))):
+    for word in words:
         control_vector = to_binary(md5sum(word))[0:length]
         for position, char in enumerate(control_vector):
-            result[position] += 1 if char == '1' else -1
+            result[position] += +1 if char == '1' else -1
     return result
 
+def generate_vectors(m, d, number_of_iterations=10):
+    ''' generates m vectors equally spread on a d-dimensional hypersphere '''
+    pass
 
 def main():
-    print(to_hash_sum_vector('34567', NUMBER_OF_VECTORS))
+    pass
+    # print(to_hash_vector(file_to_words('article'), NUMBER_OF_VECTORS))
 
 if __name__ == '__main__':
     main()
