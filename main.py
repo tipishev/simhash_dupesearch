410165fdeb7217a7670a6da184069619acb57ee7
Timofey Tipishev <tipishev@yandex-team.ru>
2015-05-24 02:29:39 +0500

    ready to walk the files

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from __future__ import print_function, unicode_literals, absolute_import
 
 __author__ = 'tipishev'
 
-# from os import walk
+from os import walk
 import md5
 from string import punctuation
 from re import sub
@@ -13,26 +13,28 @@ from random import random, seed as set_random_seed
 import logging
 from math import sqrt
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from copy import deepcopy
+from cPickle import dump, load
 
-# filesystem stuff
+
+# files stuff
 LOGFILE = "main.log"
 DOCS_DIRECTORY = "../docs/"
+VECTORS_FILE = 'vectors.pkl'
+DISTANCES_FILE = 'distances.pkl'
+DOCSTATS_FILE = 'docstats.pkl'
 
 # algorithm stuff
 RANDOM_SEED = 42
-NUMBER_OF_DIMENSIONS = 64
-NUMBER_OF_VECTORS = 8
-
-
-def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    return [(a, b) for a in A for b in B]
-
+NUMBER_OF_DIMENSIONS = 42  # a.k.a. N
+NUMBER_OF_VECTORS = 64  # a.k.a. M
 
 def list_without(list_, index):
     return list_[:index] + list_[(index+1):]
 
+
+# string stuff
 
 def md5sum(string):
     return md5.new(string.encode('utf8')).hexdigest()
@@ -69,11 +71,15 @@ def to_hash_vector(words, length):
             result[position] += +1 if char == '1' else -1
     return result
 
+def to_simhash(doc_hash_vector, vectors):
+    return ''.join(['1' if scalar(doc_hash_vector, v) > 0 else '0' for v in vectors])
+
 # vector stuff
 
 
 def create_zero_vector(size):
-    return [0 for _ in range(size)]
+    # return [0 for _ in range(size)]
+    return [0]*size
 
 
 def normalize(vector):
@@ -88,6 +94,8 @@ def to_vector(point1, point2):
 def add(vector1, vector2):
     return [sum(e) for e in zip(vector1, vector2)]
 
+def scalar(vector1, vector2):
+    return sum([e[0]*e[1] for e in zip(vector1, vector2)])
 
 def scale(vector, k):
     return [k*e for e in vector]
@@ -107,6 +115,7 @@ def get_random_vector(n):
     vector = [random() for _ in range(n)]
     return vector
 
+
 def get_coulombs_force(pusher, pushee):
     CHARGE1 = CHARGE2 = 1
     K = 0.001
@@ -119,18 +128,22 @@ def generate_random_unit_vectors(m, n):
     ''' generates m n-dimensional normal vectors '''
     return [normalize(get_random_vector(n)) for _ in range(m)]
 
+
 def spread_vectors(vectors, number_of_iterations=50):
     '''spreads vectors evenly on an n-dimensional hypersphere '''
 
     for iteration in range(number_of_iterations):
+        print("iteration #{} of {}".format(iteration, number_of_iterations))
 
         snapshot = deepcopy(vectors)  # preserve the past to build the future
         for index, old_vector in enumerate(snapshot):
             total_force = create_zero_vector(len(old_vector))
             for other_vector in list_without(snapshot, index):
-                force = get_coulombs_force(pusher=other_vector, pushee=old_vector)
+                force = get_coulombs_force(pusher=other_vector,
+                                           pushee=old_vector)
                 total_force = add(total_force, force)
-                force = get_coulombs_force(pusher=reflect(other_vector), pushee=old_vector)
+                force = get_coulombs_force(pusher=reflect(other_vector),
+                                           pushee=old_vector)
                 total_force = add(total_force, force)
             vectors[index] = normalize(add(old_vector, total_force))
     return vectors
@@ -138,12 +151,11 @@ def spread_vectors(vectors, number_of_iterations=50):
 
 def plot_3d_vectors(vectors, title='vectors'):
     x, y, z = zip(*vectors)
-    plt.title(title)
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.plot(x, y, 'ro')
-    plt.axis([-2, 2, -2, 2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(x, y, z)
     plt.show()
+
 
 def plot_2d_vectors(vectors, title='vectors'):
     x, y = zip(*vectors)
@@ -155,18 +167,50 @@ def plot_2d_vectors(vectors, title='vectors'):
     plt.show()
 
 
-def main():
-    vectors = generate_random_unit_vectors(17, 2)
-    plot_2d_vectors(vectors, title='Before spread')
-    vectors = spread_vectors(vectors, number_of_iterations=500)
-    plot_2d_vectors(vectors, title='After spread')
-    # pusher = [0.3, 0.2]
-    # pushee = [0.6, 0.1]
-    # force = get_coulombs_force(pusher, pushee)
-    # print(force)
-    # plot_vectors([pusher, pushee, force, [0,0]])
+def dump_vectors(m=NUMBER_OF_VECTORS, n=NUMBER_OF_DIMENSIONS, filename=VECTORS_FILE):
+    ''' generate m well-spread n-dimensional vectors and dump 'em to a file '''
+    vectors = generate_random_unit_vectors(m=m, n=n)
+    vectors = spread_vectors(vectors, number_of_iterations=100)
+    with open(filename, 'wb') as f:
+        dump(vectors, f)
+    log.info('saved the vectors')
 
-    # print(to_hash_vector(file_to_words('article'), NUMBER_OF_VECTORS))
+def load_vectors(filename=VECTORS_FILE):
+    with open(filename, 'rb') as f:
+        log.info('loaded the vectors')
+        return load(f)
+
+def dump_distances(distances, filename=DISTANCES_FILE):
+    ''' save a dict with hamming distances between files to a file (poor man's hash) '''
+    with open(filename, 'wb') as f:
+        dump(distances, f)
+    log.info('saved the distances')
+
+def load_distances(filename=DISTANCES_FILE):
+    with open(filename, 'rb') as f:
+        log.info('loaded the distances')
+        return load(f)
+
+def dump_docstats(docstats, filename=DOCSTATS_FILE):
+    ''' save a dict with docstats for each file '''
+    with open(filename, 'wb') as f:
+        dump(docstats, f)
+    log.info('saved the docstats')
+
+def load_docstats(filename=DOCSTATS_FILE):
+    with open(filename, 'rb') as f:
+        log.info('loaded the docstats')
+        return load(f)
+
+def handle_file(filename, vectors):
+    words = file_to_words(filename)
+    hash_vector = to_hash_vector(words=words, length=len(vectors))
+    return {'wordcount': len(words), 'simhash': to_simhash(hash_vector, vectors)}
+
+def main():
+    # vectors = load_vectors()
+    
+
 
 if __name__ == '__main__':
     logging.basicConfig(filename=LOGFILE, level=logging.INFO)
